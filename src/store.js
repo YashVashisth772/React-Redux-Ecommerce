53f57c485a1fd3b00329548db7e990206d250f2e
Yash Vashisth <42314417+YashVashisth772@users.noreply.github.com>
2021-07-01 20:53:43 +0530

    all files

---
+++ src/store.js
@@ -0,0 +1,17 @@
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import rootReducers from "./reducers";
+import logger from 'redux-logger';
+
+
+const cartItems = localStorage.getItem("cartItems")
+  ? JSON.parse(localStorage.getItem("cartItems"))
+  : [];
+const initState = { cart: { items: cartItems } };
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+  rootReducers,
+  initState,
+  composeEnhancer(applyMiddleware(thunk,logger))
+);
+export default store;
